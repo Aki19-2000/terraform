e815ba371812e753924bda066d99d55efef2a708
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-18 18:14:11 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "akired-${random_string.bucket_suffix.result}"
-    key    = "terraform.tfstate"
-    region = "us-east-1"  # Ensure this matches your intended region
-    encrypt = true
-  }
-}
