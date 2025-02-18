2008efad706153a11cdfb18b0dd2ce4516830e7a
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-18 15:43:18 +0530

    Update remote_state.tf

--- remote_state.tf
+++ remote_state.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-unique"  # Ensure this is a globally unique name
     key            = "state/terraform.tfstate"
-    region         = "eu-west-3"  # Set this to the correct region where your resources are located
+    region         = 'us-east-1'  # Set this to the correct region where your resources are located
     dynamodb_table = "my-terraform-lock-table"  # Ensure this matches the region as well
     encrypt        = true
   }
