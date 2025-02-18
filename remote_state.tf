82caf4d46af0fb2fce5f40debd123725506886ef
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-18 15:53:56 +0530

    Delete remote_state.tf

--- remote_state.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket-unique"  # Ensure this is a globally unique name
-    key            = "state/terraform.tfstate"
-    region         = 'us-east-1'  # Set this to the correct region where your resources are located
-    dynamodb_table = "my-terraform-lock-table"  # Ensure this matches the region as well
-    encrypt        = true
-  }
-}
