09582aa497ad54a09901038428a51d8ed60c9d34
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-18 16:19:39 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,8 @@
-# backend.tf
-
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state"
+    bucket         = "my-terraform-state"  # Should match the S3 bucket you create
     key            = "vpc-terraform.tfstate"
-    region         = "us-east-1"  # Changed to us-east-1
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"       # Should match the DynamoDB table you create
   }
 }
