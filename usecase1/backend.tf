ea3c8ca2013774e4a36a052457b4c21f0c120556
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-18 19:01:15 +0530

    Update backend.tf

--- usecase1/backend.tf
+++ usecase1/backend.tf
@@ -4,6 +4,6 @@ terraform {
     bucket         = "akired"
     key            = "env:/dev/terraform.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "usecase1"
+    dynamodb_table = "terraform-locks"
   }
 }
