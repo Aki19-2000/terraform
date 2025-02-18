821376ad706b700717da52227e7f5472eeb3f0b6
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-18 15:25:22 +0530

    Update remote_state.tf

--- remote_state.tf
+++ remote_state.tf
@@ -1,9 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket-unique"  # Use the actual name of your created S3 bucket
-    key            = "state/terraform.tfstate"
-    region         = "us-east-1"  # Same region as your S3 bucket and DynamoDB table
-    dynamodb_table = "my-terraform-lock-table"  # Use the name of your created DynamoDB table
-    encrypt        = true
-  }
+  # Remove the backend block temporarily
+  # backend "s3" { ... }
 }
