bc675e86b4f51a2be817699541143f8edb196140
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-18 20:06:33 +0530

    Update backend.tf

--- usecase1/backend.tf
+++ usecase1/backend.tf
@@ -1,7 +1,7 @@
 # Configure Terraform Backend.
 terraform {
   backend "s3" {
-    bucket         = "akired"
+    bucket         = "akired1"
     key            = "env:/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
